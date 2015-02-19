919e2c7cf2cfbf5badee40f2cf9d3abdcd781502
Eric Wieser <efw27@cam.ac.uk>
2015-02-19 10:17:09 +0000

    Use new port class in arm code

--- arm.cc
+++ arm.cc
@@ -2,26 +2,15 @@
 #include "arm.h"
 #include "ports.h"
 
-const int PIN_UP_DOWNB = 0;
-const int PIN_OPEN_CLOSEB = 1;
-
 void Arm::up() {
-	uint8_t word = _r->request(ports::read_instr(_port));
-	word |= (1 << PIN_UP_DOWNB);
-	_r->command(ports::write_instr(_port, word));
+	_port |= (1 << PIN_UP_DOWNB);
 }
 void Arm::down() {
-	uint8_t word = _r->request(ports::read_instr(_port));
-	word &= ~(1 << PIN_UP_DOWNB);
-	_r->command(ports::write_instr(_port, word));
+	_port &= ~(1 << PIN_UP_DOWNB);
 }
 void Arm::open() {
-	uint8_t word = _r->request(ports::read_instr(_port));
-	word |= (1 << PIN_OPEN_CLOSEB);
-	_r->command(ports::write_instr(_port, word));
+	_port |= (1 << PIN_OPEN_CLOSEB);
 }
 void Arm::close() {
-	uint8_t word = _r->request(ports::read_instr(_port));
-	word &= ~(1 << PIN_OPEN_CLOSEB);
-	_r->command(ports::write_instr(_port, word));
+	_port &= ~(1 << PIN_OPEN_CLOSEB);
 }
--- arm.h
+++ arm.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "device.h"
+#include "ports.h"
 
 class Arm : public Device {
+	Port _port;
 public:
+	enum {
+		PIN_UP_DOWNB = 0,
+		PIN_OPEN_CLOSEB = 1
+	};
 	void up();
 	void down();
 	void open();
 	void close();
 
-    Arm(RLink* r) : Device(r) {}
+    Arm(RLink* r, port::Name name)
+    	: Device(r), _port(r, name) {}
 };
