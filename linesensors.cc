6760c3798d63595b11ce6f0a2573512289e6f487
Matt Diesel <M@ttDiesel.co.uk>
2015-02-19 12:19:20 +0000

    Removed unused variable

--- linesensors.cc
+++ linesensors.cc
@@ -25,7 +25,6 @@ LineSensors::Reading LineSensors::read() {
 
 	// Determine position
 	float pos = 0.0f;
-	bool junc = false;
 	ret.state = Reading::LINE;
 	switch (sensors & 0b111) {
 		case 0: // 000
