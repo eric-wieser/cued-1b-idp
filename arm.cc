e5919c5fa77fe58f5ddab8a54b39e7986dee6d0d
Eric Wieser <efw27@cam.ac.uk>
2015-02-19 09:44:45 +0000

    Fix pointer dereferencing

--- arm.cc
+++ arm.cc
@@ -6,22 +6,22 @@ const int PIN_UP_DOWNB = 0;
 const int PIN_OPEN_CLOSEB = 1;
 
 void Arm::up() {
-	uint8_t word = _r.request(ports::read_instr(_port));
+	uint8_t word = _r->request(ports::read_instr(_port));
 	word |= (1 << PIN_UP_DOWNB);
-	_r.command(ports::write_instr(_port, word));
+	_r->command(ports::write_instr(_port, word));
 }
 void Arm::down() {
-	uint8_t word = _r.request(ports::read_instr(_port));
+	uint8_t word = _r->request(ports::read_instr(_port));
 	word &= ~(1 << PIN_UP_DOWNB);
-	_r.command(ports::write_instr(_port, word));
+	_r->command(ports::write_instr(_port, word));
 }
 void Arm::open() {
-	uint8_t word = _r.request(ports::read_instr(_port));
+	uint8_t word = _r->request(ports::read_instr(_port));
 	word |= (1 << PIN_OPEN_CLOSEB);
-	_r.command(ports::write_instr(_port, word));
+	_r->command(ports::write_instr(_port, word));
 }
 void Arm::close() {
-	uint8_t word = _r.request(ports::read_instr(_port));
+	uint8_t word = _r->request(ports::read_instr(_port));
 	word &= ~(1 << PIN_OPEN_CLOSEB);
-	_r.command(ports::write_instr(_port, word));
+	_r->command(ports::write_instr(_port, word));
 }
