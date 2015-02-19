887e1670cea9f081b1cdcf1c00fa77f67f6d0496
Matt Diesel <M@ttDiesel.co.uk>
2015-02-19 11:42:53 +0000

    Main now generic.
    Use robotLogic entry point in a test file.

--- main.cc
+++ main.cc
@@ -1,25 +1,13 @@
-#include "rlink.h"
-#include "robot_instr.h"
-#include "robot_delay.h"
-#include <iostream>
 
-#include "drive.h"
+#include "robot.h"
+#include "timing.h"
 
 int main() {
-	RLink r;
-	std::cout << "constructed" << std::endl;
-	r.initialise();
-	std::cout << "initialised" << std::endl;
+	for (;;) {
+		timing::tick();
 
-	Drive drive(&r);
-
-	float f = 1;
-	while(true) {
-		std::cout << "ready..." << std::endl;
-		std::cin >> f;
-		std::cout << "going" << std::endl;
-		drive.move({forward: f, steer: 0});
+		robotLogic();
 	}
 
-	std::cout << "done" << std::endl;
+	return(0);
 }
