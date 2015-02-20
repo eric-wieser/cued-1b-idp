d2ce04ca89cb1d8fb51102d3ab37055b4a82fd46
Eric Wieser <wieser.eric@gmail.com>
2015-02-20 14:39:28 +0000

    Add error-checking code at startup, and exception catching/reinitialization in main loop.

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include "robot.h"
 #include "timing.h"
 #include "rlink.h"
+#include "logging.h"
 
 void robotLogic(Robot& robot) {
 
@@ -13,15 +14,43 @@ int main() {
 	RLink rlink;
 
 	std::cout << "constructed" << std::endl;
-	rlink.initialise();
-	std::cout << "initialised" << std::endl;
+	try {
+		rlink.initialise();
+	catch (LinkError& e) {
+		ERR(std::cout << "link error: initialization failed" << endl);
+		ERR(e.log());
+		return -1;
+	}
+
+	try {
+		if (rlink.request (TEST_INSTRUCTION) != TEST_INSTRUCTION_RESULT) {
+			ERR(std::cout << "test instruction failed" << endl);
+			return -1;
+		}
+	}
+	catch (LinkError& e) {
+		ERR(std::cout << "link error: test instruction" << endl);
+		ERR(e.log());
+		return -1;
+	}
+
+	INFO(std::cout << "initialised" << std::endl);
 
 	Robot robot(rlink);
 
 	for (;;) {
 		timing::tick();
-
-		robotLogic(robot);
+		try {
+			robotLogic(robot);
+		} catch (LinkError& e) {
+			WARN(std::cout << "Unhandled link error" << std::endl);
+			WARN(e.log());
+			if(e.is_fatal) {
+				WARN(std::cout << "Requires reinitialization" << std::endl);
+				rlink.reinitialize();
+				INFO(std::cout << "Reinitialized" << std::endl);
+			}
+		}
 	}
 
 	return(0);
