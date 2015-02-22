b367dd08e121eccc163dd143fc2e59ad75d3ddd5
Eric Wieser <wieser.eric@gmail.com>
2015-02-22 11:03:29 +0000

    Comment on egg locations

--- eggtype.h
+++ eggtype.h
@@ -1,8 +1,8 @@
 #pragma once
 
 enum EggType {
-	EGG_WHITE,
-	EGG_BROWN,
-	EGG_TASTY,
+	EGG_WHITE,  // deliver to D1
+	EGG_BROWN,  // deliver to D2
+	EGG_TASTY,  // deliver to D3
 	EGG_NONE = -1
 };
