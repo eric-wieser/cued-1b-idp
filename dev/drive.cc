6019f9a659eccf58d30daf5a185eca2cca61b825
Eric Wieser <efw27@cam.ac.uk>
2015-02-23 15:58:00 +0000

    Fix incorrect casting of float to int, and signs

--- dev/drive.cc
+++ dev/drive.cc
@@ -1,9 +1,10 @@
 #include "drive.h"
 #include "robot_instr.h"
 #include <cstdint>
+#include <cmath>
 
 uint8_t Drive::convertSpeed(float s) {
-	uint8_t res = std::min(abs(s), 1) * 127;
+	uint8_t res = std::min(fabs(s), 1.0) * 127;
 
 	if(s < 0) res |= 0x80;
 
@@ -19,7 +20,7 @@ void Drive::move(move_args args) {
 void Drive::setWheelSpeeds(float left, float right) {
 	// apply any sign corrections here
 	uint8_t left_i = convertSpeed(left);
-	uint8_t right_i = convertSpeed(right);
+	uint8_t right_i = convertSpeed(-right);
 
 	if (left_i == right_i) {
 		_r.command(BOTH_MOTORS_GO_SAME, left_i);
