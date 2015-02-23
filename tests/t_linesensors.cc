8c354735a52a1d613c39322026fc1c9c3110b81a
Eric Wieser <efw27@cam.ac.uk>
2015-02-23 16:11:29 +0000

    Simple test of line sensor code

---
+++ tests/t_linesensors.cc
@@ -0,0 +1,21 @@
+#include "../dev/rlink.h"
+#include "../dev/linesensors.h"
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+int main() {
+	RLink r;
+	std::cout << "constructed" << std::endl;
+	r.initialise();
+	std::cout << "initialised" << std::endl;
+
+	LineSensors ls(r, port::P1);
+
+	while(1) {
+		auto reading = ls.read();
+		std::cout << "line position: " << reading.position << "       \r";
+		std::cout.flush();
+	}
+	return 0;
+}
