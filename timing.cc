80a18d59f12e626b89757500f0d1089645b2d174
Matt Diesel <M@ttDiesel.co.uk>
2015-02-25 00:30:52 +0000

    Added chrono timer wrapping functions.

--- timing.cc
+++ timing.cc
@@ -13,3 +13,18 @@ ticks_t ticks() {
 void timing::tick() {
 	timing::_ticks++;
 }
+
+ 
+timer_t timing::now() {
+    _timerClock::time_point t1 = _timerClock::now();
+ 
+	return timer_t(std::chrono::duration_cast<std::chrono::nanoseconds>
+			(t1.time_since_epoch()).count());
+}
+ 
+double timing::diff(timer_t start) {
+	_timerClock::time_point t1 = _timerClock::now();
+ 
+	return double(timer_t(std::chrono::duration_cast<std::chrono::nanoseconds>
+			(t1.time_since_epoch()).count()) - start) / 1000000.0;
+}
