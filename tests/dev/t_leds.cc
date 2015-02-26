e702509ad019f1a1ef5a444cc055ce8b5820805e
Eric Wieser <efw27@cam.ac.uk>
2015-02-26 10:57:33 +0000

    add led test

---
+++ tests/dev/t_leds.cc
@@ -0,0 +1,31 @@
+#include "dev/ports.h"
+#include "dev/rlink.h"
+#include <cstdint>
+#include <iostream>
+
+#include "robot_delay.h"
+
+int main() {
+	RLink r;
+	std::cout << "constructed" << std::endl;
+	r.initialise();
+	std::cout << "initialised" << std::endl;
+
+	Port leds(r, port::P0, 0x07);
+	Port button(r, port::P0, 0x08);
+	button = 0x08;
+
+	std::cout << "constructed ports" << std::endl;
+
+	int i = 0;
+	while(true) {
+		int last = -1;
+		while(!button);
+		delay(250);
+		while(button);
+		i++;
+
+		leds = 1 << (i % 4);
+		button = 0x08;
+	}
+}
