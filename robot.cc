9467c167672bed845d5da0de770c836e999fb74c
MattDiesel <M@ttDiesel.co.uk>
2015-03-02 11:31:12 +0000

    Updated port list

--- robot.cc
+++ robot.cc
@@ -5,40 +5,43 @@
 /*
 Port mappings:
 
-Port 0 - reserved for sensor test rig
+Port 0
+    Reserved for sensor test rig
 
 Port 1 - line and egg sensors
-	0  LSL  Line Sensor Left
-	1  LSC  Line Sensor Centre
-	2  LSR  Line Sensor Right
-	3  LSA  Line Sensor Arm
+    0  LSL  Line Sensor Left
+    1  LSC  Line Sensor Centre
+    2  LSR  Line Sensor Right
+    3  LSA  Line Sensor Arm
 
-	4  ELR	Egg LED Red
-	5  ELG  Egg LED Green
-	6  ELB  Egg LED Blue
-	7  ---
+    4  ELR    Egg LED Red
+    5  ELG  Egg LED Green
+    6  ELB  Egg LED Blue
+    7  ELO  Egg LDR sensor output
 
 Port 2 - arm
-	0  APL  Arm Pivot Lowered
-	1  APR  Arm Pivot Raised
-	2  AGE  Arm Grab Extended
-	3  AGR  Arm Grab Retracted
 
-	4  APA  Arm Pivot Actuator
-	5  AGA  Arm Grab Actuator
-	6  ---
-	7  BSF  Box Switch Front
+    0  APL  Arm Pivot Lowered
+    1  APR  Arm Pivot Raised
+    2  AGE  Arm Grab Extended
+    3  AGR  Arm Grab Retracted
+    4  DR  delivery raised
+    5  BSF  Box Switch Front
+    6  APA  Arm Pivot Actuator
+    7  AGA  Arm Grab Actuator
+   
 
-Port 3 - courier indicator leds
-	0  C1W  Courier bottom - white egg indicator LED
-	1  C1B  Courier bottom - brown egg indicator LED
-	2  C1T  Courier bottom - tasty egg indicator LED
-	3  C2W  Courier middle - white egg indicator LED
+Port 3 - courier indicator LEDs & Lightgate
 
-	4  C2B  Courier middle - brown egg indicator LED
-	5  C2T  Courier middle - tasty egg indicator LED
-	6  C3B  Courier top - brown egg indicator LED
-	7  C3B  Courier top - tasty egg indicator LED
+    0  C1W  Courier bottom - White egg indicator LED
+    1  C1R  Courier bottom - Red egg indicator LED
+    2  C2W  Courier Middle - white egg indicator LED
+    3  C2R  Courier middle - Red egg indicator LED
+    4  C3W  Courier top - white egg indicator LED
+    5  C3R  Courier top - Red egg indicator LED
+
+    6  LGO Lightgate Output
+    7  LGI light gate indicator
 */
 Robot::Robot(RLink& rlink) :
 	drive(rlink, {
