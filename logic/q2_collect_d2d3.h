e524b1f0ece548420ad1d619f91c699e939b2ce2
Matt Diesel <M@ttDiesel.co.uk>
2015-03-04 19:10:25 +0000

    Updated Q2 sequence with changes to mechanics

--- logic/q2_collect_d2d3.h
+++ logic/q2_collect_d2d3.h
@@ -3,10 +3,14 @@
 
 #include "../robot.h"
 #include "sequence.h"
+#include "../eggtype.h"
 
 
 class q2_collect_d2d3 : public StateMachine
 {
+private:
+	EggType currentEgg;
+	int conveyorPos;
 public:
 	q2_collect_d2d3(Robot& robot);
 
