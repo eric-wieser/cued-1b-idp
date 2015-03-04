220dceb0b20ec7215c95129af24d304696ca34a6
Matt Diesel <M@ttDiesel.co.uk>
2015-03-04 18:45:53 +0000

    Removed duplicate definitions from linefollow test

--- tests/t_linefollow_gl.cc
+++ tests/t_linefollow_gl.cc
@@ -7,41 +7,8 @@
 #include "../logging.h"
 #include "../timing.h"
 #include "../dev/rlink.h"
+#include "../util/signals.h"
 
-/** Delays the rising edge of a boolean signal.
-
-         Original Signal                              Transient shorter than ticks ignored
-       /                                             / 
-     ..........__________________________          .....
-     :        |  \                       |         :   :
-     :        |   Delayed rising edge    |         :   :
-_____:________|                          |_________:___:_______
-      <------>                            \
-        ticks                              No delay on falling edge.
-
-*/
-bool delay_rising(int& hist, bool value, int ticks = 5) {
-	if (value) {
-		hist++;
-		return( hist >= ticks);
-	}
-
-	hist = 0;
-	return( false );
-}
-
-/** Delays the falling edge of a boolean signal
-(as for delay_rising but for falling edge.)
-*/
-bool delay_falling(int& hist, bool value, int ticks = 5) {
-	if (value) {
-		hist = 0;
-		return( true );
-	}
-
-	hist++;
-	return( hist < ticks);
-}
 
 struct robotLogic
 {
