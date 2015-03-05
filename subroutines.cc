68e1943266759a67b37e25e0e606f45add4f4598
Eric Wieser <efw27@cam.ac.uk>
2015-03-05 22:25:25 +0000

    Deliver 3 eggs in functionality demo. Use limit switches to find boxes
    Needs more work. Todo:
     - Time motion before limit switch hit, so that it can be reversed (make sure we don't reverse off the table)
     - Handle different egg orders

--- subroutines.cc
+++ subroutines.cc
@@ -83,7 +83,43 @@ void demos::f2(Robot& r) {
 	std::cout << "At delivery nexus" << std::endl;
 }
 
+static void dropCreme(Robot& r) {
+	r.drive.move({forward: 0.5, steer: 0});
+	while(!(r.limits & (1 << 5)));
+	r.drive.stop();
+
+	std::cout << "At D3" << std::endl;
+	r.courier.unloadEgg();
+
+	r.drive.straight(-0.1, 0.5).wait();
+
+}
+
+static void dropWhite(Robot& r) {
+	r.drive.move({forward: 0.7, steer: 1});
+
+	while(r.ls.read().lsc);
+	while(!r.ls.read().lsc);
+	r.drive.stop();
+
+	r.drive.move({forward: 0.5, steer: 0});
+	while(!(r.limits & (1 << 5)));
+	r.drive.stop();
+
+	std::cout << "At D2" << std::endl;
+	r.courier.unloadEgg();
+
+	r.drive.straight(-0.05, 0.5).wait();
+
+	// rotate back to nexus, then drive to D3
+	r.drive.move({forward: -0.7, steer: -1});
+	while(r.ls.read().state == LineSensors::Reading::LINE);
+	while(r.ls.read().state != LineSensors::Reading::LINE);
+	r.drive.stop();
+}
+
 void demos::f3(Robot& r) {
+	std::cout << "At delivery nexus" << std::endl;
 	Tracker::instance()->logPose(-1.2 + 0.36 + 0.2, 1.2 - 0.36, M_PI);
 	// add some virtual eggs if we don't yet have any
 	if(r.courier.volume() < 3) r.courier.recordEggAdded(EGG_BROWN);
@@ -91,25 +127,10 @@ void demos::f3(Robot& r) {
 
 	// rotate to D2 delivery
 	r.drive.straight(0.1).wait();
-	r.drive.move({forward: 0.7, steer: 1});
 
-	while(r.ls.read().lsc);
-	while(!r.ls.read().lsc);
-	r.drive.stop();
-	std::cout << "At D2" << std::endl;
-	r.courier.unloadEgg();
-
-	// rotate back to nexus, then drive to D3
-	r.drive.move({forward: -0.7, steer: -1});
-	while(r.ls.read().state == LineSensors::Reading::LINE);
-	while(r.ls.read().state != LineSensors::Reading::LINE);
-	r.drive.stop();
-
-	delay(1000);
-	r.drive.straight(0.05).wait();
-	r.drive.stop();
-	std::cout << "At D3" << std::endl;
-	r.courier.unloadEgg();
+	dropWhite(r);
+	dropCreme(r);
+	dropWhite(r);
 }
 
 void demos::all(Robot& r) {
