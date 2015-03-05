a95277cf5e99b4548f428774f8518c394a6a216c
Eric Wieser <efw27@cam.ac.uk>
2015-03-05 14:29:14 +0000

    Test both actuators in sequence

--- tests/dev/t_actuator.cc
+++ tests/dev/t_actuator.cc
@@ -5,13 +5,16 @@
 
 #include "robot_delay.h"
 
+const int lift = (1 << 6);
+const int grab = (1 << 7);
+
 int main() {
 	RLink r;
 	std::cout << "constructed" << std::endl;
 	r.initialise();
 	std::cout << "initialised" << std::endl;
 
-	Port actuator(r, port::P3, (1 << 6) | (1 << 7));
+	Port actuator(r, port::P2, lift | grab);
 	actuator = 0x00;
 
 	std::cout << "constructed ports" << std::endl;
@@ -19,8 +22,16 @@ int main() {
 	int b = 0;
 	while(true) {
 		actuator = 0x00;
+		std::cout << "off" << std::endl;
 		delay(1000);
-		actuator = 0xff;
+		actuator = lift;
+		std::cout << "on1" << std::endl;
+		delay(1000);
+		actuator = lift | grab;
+		std::cout << "on2" << std::endl;
+		delay(1000);
+		actuator = grab;
+		std::cout << "on3" << std::endl;
 		delay(1000);
 	}
 }
