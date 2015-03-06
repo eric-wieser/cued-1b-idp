2e07f263cb7efa7cd86187b343742dcc8baa652c
Eric Wieser <wieser.eric@gmail.com>
2015-03-06 00:03:08 +0000

    Check for I2C errors when things go wrong

--- dev/rlink.cc
+++ dev/rlink.cc
@@ -18,3 +18,10 @@ int RLink::request(request_instruction req) {
 	if(res == REQUEST_ERROR) throw LinkError(*this);
 	return res;
 }
+uint8_t RLink::status() {
+	int res = robot_link::request(STATUS);
+	// error while checking for an error? It's a comms error - bit 0
+	if(res == REQUEST_ERROR) return 1;
+
+	return res;
+}
--- dev/rlink.h
+++ dev/rlink.h
@@ -2,26 +2,37 @@
 
 #include <robot_link.h>
 
-class LinkError;
-
 /**
 	Wraps robot_link to indicate failures with exceptions
- */
+
+	.status() is special, as error checking should not itself throw an exception
+*/
 class RLink : public robot_link {
 public:
 	void initialise();
 	void command(command_instruction cmd, int arg);
 	int request(request_instruction req);
+	uint8_t status();
 };
 
+/**
+	Thrown when an RLink command or request goes wrong. Contains the original
+	error code
+*/
 class LinkError : public std::exception {
 private:
 	RLink& _r;
 public:
 	const link_err err;
 	const bool is_fatal;
+	const bool is_i2c;
 
-	LinkError(RLink& r) : _r(r), err(r.lookup_err(0)), is_fatal(r.fatal_err()) {}
+	LinkError(RLink& r) :
+		_r(r),
+		err(r.lookup_err(0)),
+		is_fatal(r.fatal_err()),
+		is_i2c(r.status() & 0x2) {}
+
 	virtual ~LinkError() throw() { _r.clear_errs(); }
 	inline void log() {
 		_r.print_errs("\t");
@@ -29,6 +40,8 @@ public:
 
 	/// override of std::exception
 	virtual const char* what() const throw() {
-		return err == LINKERR_NONE ? "Undescribed error" : _r.err_string(err);
+		if(err != LINKERR_NONE) return _r.err_string(err);
+		if(is_i2c) return "I2C bus error - board unplugged?";
+		return "No error code available";
 	}
 };
