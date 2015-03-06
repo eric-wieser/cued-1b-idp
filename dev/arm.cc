6f77dd244dcbb1f0da4aed40968dd2be5c7b0c93
MattDiesel <M@ttDiesel.co.uk>
2015-03-06 13:42:41 +0000

    Added delays to allow actuators to complete movement

--- dev/arm.cc
+++ dev/arm.cc
@@ -1,5 +1,7 @@
 #include <cstdint>
 #include "arm.h"
+#include <robot_delay.h>
+
 #include "ports.h"
 
 
@@ -12,13 +14,17 @@ Arm::Arm(RLink& r, port::Name name) :
 
 void Arm::down() {
 	_port |= (1 << PIN_DOWN_UPB);
+	delay(2000);
 }
 void Arm::up() {
 	_port &= ~(1 << PIN_DOWN_UPB);
+	delay(2000);
 }
 void Arm::close() {
 	_port |= (1 << PIN_CLOSED_OPENB);
+	delay(1000);
 }
 void Arm::open() {
 	_port &= ~(1 << PIN_CLOSED_OPENB);
+	delay(1000);
 }
