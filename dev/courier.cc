9d434e295bd98630d2597530a52c0e351256b334
Eric Wieser <efw27@cam.ac.uk>
2015-03-09 15:51:37 +0000

    Fix active-lowness of egg indicators

--- dev/courier.cc
+++ dev/courier.cc
@@ -13,29 +13,30 @@ static const float liftSpeed = -1;
 Courier::Courier(RLink& r, port::Name ledPort)
 		: Device(r), _ledPort(r, ledPort), _volume(0)
 {
+	_contents[0] = _contents[1] = _contents[2] = EGG_NONE;
 	r.command(MOTOR_3_GO, Drive::convertSpeed(holdSpeed));
 }
 
 void Courier::_updateLeds() {
-	uint8_t mask = 0;
+	uint8_t mask = 0xFF;
 
-	switch(_contents[0]) {
-		case EGG_WHITE: mask |= 0b000001; break;
-		case EGG_BROWN: mask |= 0b000010; break;
-		case EGG_TASTY: mask |= 0b000011; break;
+	switch(_contents[2]) {
+		case EGG_WHITE: mask &= ~0b000001; break;
+		case EGG_BROWN: mask &= ~0b000010; break;
+		case EGG_TASTY: mask &= ~0b000011; break;
 	}
 
 
 	switch(_contents[1]) {
-		case EGG_WHITE: mask |= 0b000100; break;
-		case EGG_BROWN: mask |= 0b001000; break;
-		case EGG_TASTY: mask |= 0b001100; break;
+		case EGG_WHITE: mask &= ~0b000100; break;
+		case EGG_BROWN: mask &= ~0b001000; break;
+		case EGG_TASTY: mask &= ~0b001100; break;
 	}
 
-	switch(_contents[2]) {
-		case EGG_WHITE: mask |= 0b010000; break;
-		case EGG_BROWN: mask |= 0b100000; break;
-		case EGG_TASTY: mask |= 0b110000; break;
+	switch(_contents[0]) {
+		case EGG_WHITE: mask &= ~0b010000; break;
+		case EGG_BROWN: mask &= ~0b100000; break;
+		case EGG_TASTY: mask &= ~0b110000; break;
 	}
 
 	_ledPort = mask;
@@ -44,8 +45,8 @@ void Courier::_updateLeds() {
 void Courier::recordEggAdded(EggType egg_t) {
 	assert(_volume < 3);
 
-	_contents[++_volume] = egg_t;
-	// _updateLeds();
+	_contents[_volume++] = egg_t;
+	_updateLeds();
 }
 
 void Courier::unloadEgg() {
@@ -55,14 +56,17 @@ void Courier::unloadEgg() {
 	_r.command(MOTOR_3_GO, Drive::convertSpeed(dropSpeed));
 	delay(1000);
 	_r.command(MOTOR_3_GO, Drive::convertSpeed(liftSpeed));
-	delay(2000);
-	_r.command(MOTOR_3_GO, Drive::convertSpeed(holdSpeed));
 
 	// update internal state, if we succeed
 	_contents[0] = _contents[1];
 	_contents[1] = _contents[2];
 	_contents[2] = EGG_NONE;
-	// _updateLeds();
+	_volume--;
+	_updateLeds();
+
+	delay(2000);
+	_r.command(MOTOR_3_GO, Drive::convertSpeed(holdSpeed));
+
 }
 
 int Courier::volume() { return _volume; }
