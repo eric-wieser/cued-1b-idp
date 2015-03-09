0b320a9cd79734907b761016eaa803aca503dfc0
MattDiesel <M@ttDiesel.co.uk>
2015-03-09 12:28:13 +0000

    Renamed subroutines-lowlevel to linefollow

---
+++ routines/linefollow.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+
+
+struct LineLost : public std::exception {
+	const LineSensors::Reading lastReading;  ///< Reading that indicated a lost line
+	const float distanceLeft;                ///< distance left along the line when lost
+
+	LineLost(LineSensors::Reading r, float d) : lastReading(r), distanceLeft(d) {};
+
+	/// override of std::exception::what
+	virtual const char* what() const throw();
+};
+
+
+struct HardwareDamaged : public std::exception {};
+
+enum GOTOJUNC_RET {
+	RET_JUNCTION,
+	RET_TIMEOUT
+};
+
+GOTOJUNC_RET goToJunction(Robot& r, float distance);
+void turnAtJunction(Robot& r);
