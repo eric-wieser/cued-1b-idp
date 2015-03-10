445fb714a538af425a80cee7e6649a43575fe03e
Eric Wieser <efw27@cam.ac.uk>
2015-03-10 18:24:33 +0000

    Fix arguments to turnAtJunction

--- routines/q3_return_from_d2d3.cc
+++ routines/q3_return_from_d2d3.cc
@@ -2,16 +2,15 @@
 #include "subroutines.h"
 
 void q3_return_from_d2d3(Robot& r) {
-	turnAtJunction(r, 90);
-	turnAtJunction(r, 90);
+	turnAtJunction(r, 2);
 	goToJunction(r, 0.86);
 	goToJunction(r, 0.86);
 	std::cout << "At plateau start" << std::endl;
 
-	turnAtJunction(r, -90);
+	turnAtJunction(r, -1);
 	goToJunction(r, 99999999); // ramps takes forever, so just drop the timeout
 
-	turnAtJunction(r, -90);
+	turnAtJunction(r, -1);
 	// goToFullJunction(r, 0.84);
 	goToJunction(r, 0.84 - 0.20);
 	goToJunction(r, 0.84 - 0.20);
