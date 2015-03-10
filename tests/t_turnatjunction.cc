db84dd2d49b9461ae656c3dc6ca93e96b7d8ace7
MattDiesel <M@ttDiesel.co.uk>
2015-03-10 16:04:07 +0000

    Added turnatjunction test code

---
+++ tests/t_turnatjunction.cc
@@ -0,0 +1,22 @@
+
+#include "robot.h"
+#include "routines/subroutines.h"
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+int main() {
+	RLink link;
+	std::cout << "constructed" << std::endl;
+	link.initialise();
+	std::cout << "initialised" << std::endl;
+
+	Robot r(link);
+
+	goToJunction(r, 1);
+	std::cout << "Junction" << std::endl;
+	turnAtJunction(r, -2);
+	std::cout << "Turned" << std::endl;
+	goToJunction(r, 1);
+}
