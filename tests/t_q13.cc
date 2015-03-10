14ed8765500c32ecfb4e5ae191e3d2498477bc15
Eric Wieser <efw27@cam.ac.uk>
2015-03-10 14:47:18 +0000

    Rewrite of funcdemo

---
+++ tests/t_q13.cc
@@ -0,0 +1,34 @@
+
+#include "robot.h"
+#include "routines/subroutines.h"
+#include "util/tracker.h"
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <robot_delay.h>
+
+
+
+int main() {
+	RLink link;
+	std::cout << "constructed" << std::endl;
+	link.initialise();
+	std::cout << "initialised" << std::endl;
+
+	while(1) {
+		Robot r(link);
+
+		std::cout
+			<< "Choose a demo:" << std::endl
+			<< " 1 - it" << std::endl;
+
+		std::cout << "Your choice: ";
+		int n = -1;
+		std::cin >> n;
+
+		q2_collect_d2d3(r);
+		q2_deliver_d2d3(r);
+		q2_return_from_d2d3(r);
+	}
+}
