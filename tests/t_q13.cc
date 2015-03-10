7afe612a07231733eb6f3fd0bf2668f2cfd1f7d5
Eric Wieser <efw27@cam.ac.uk>
2015-03-10 18:21:13 +0000

    Add multiple start locations

--- tests/t_q13.cc
+++ tests/t_q13.cc
@@ -20,14 +20,23 @@ int main() {
 		Robot r(link);
 
 		std::cout
-			<< "Choose a demo:" << std::endl
-			<< " 1 - it" << std::endl;
+			<< "Choose a start position:" << std::endl
+			<< " 1 - start box" << std::endl
+			<< " 2 - last egg" << std::endl
+			<< " 3 - post-delivery" << std::endl;
 
 		std::cout << "Your choice: ";
 		int n = -1;
 		std::cin >> n;
 
-		q1_collect_d2d3(r);
+		if(n <= 1) {
+			q1_collect_d2d3(r);
+		}
+		else {
+			r.courier.recordEggAdded(EGG_WHITE);
+			r.courier.recordEggAdded(EGG_TASTY);
+			r.courier.recordEggAdded(EGG_WHITE);
+		}
 		q2_deliver_d2d3(r);
 		q3_return_from_d2d3(r);
 	}
