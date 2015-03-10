e317df75d83121af2980def3480227f23fc46105
MattDiesel <M@ttDiesel.co.uk>
2015-03-10 16:37:06 +0000

    Added outline for q6

--- routines/q6_return_from_d1.cc
+++ routines/q6_return_from_d1.cc
@@ -3,4 +3,11 @@
 #include "subroutines.h"
 
 void q6_return_from_d1(Robot& r) {
+	turnAtJunction(r, 1);
+	goToJunction(r, 0.30);
+	turnAtJunction(r, 1);
+	goToJunction(r, 0.50);
+	goToJunction(r, 0.10);
+	turnAtJunction(r, 1);
+	// Reverse?
 }
