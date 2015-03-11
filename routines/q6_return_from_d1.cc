f39b1f98910d1f2706850025a287ab32790f99a3
Eric Wieser <efw27@cam.ac.uk>
2015-03-11 18:40:44 +0000

    Decrease chances of timeouts

--- routines/q6_return_from_d1.cc
+++ routines/q6_return_from_d1.cc
@@ -3,11 +3,10 @@
 #include "subroutines.h"
 
 void q6_return_from_d1(Robot& r) {
-	r.drive.straight(-0.2).wait();
-	turnAtJunction(r, 1);
-	goToJunction(r, 0.84);
+	turnAtJunction(r, 1, false);
+	goToJunction(r, 1);
 	turnAtJunction(r, 1);
 	goToJunction(r, 0.70);
-	goToJunction(r, 0.15);
+	goToJunction(r, 0.25);
 	r.drive.straight(0.1).wait();
 }
