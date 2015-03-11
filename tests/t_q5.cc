dfa1627400ad78ed7c05b0960710e22a8cf4c151
MattDiesel <M@ttDiesel.co.uk>
2015-03-11 10:50:36 +0000

    Added Q5 test code

---
+++ tests/t_q5.cc
@@ -0,0 +1,23 @@
+
+#include "robot.h"
+#include "routines/subroutines.h"
+#include "util/tracker.h"
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <robot_delay.h>
+
+
+
+int main() {
+	RLink link;
+	std::cout << "constructed" << std::endl;
+	link.initialise();
+	std::cout << "initialised" << std::endl;
+
+	Robot r(link);
+
+	// q4_collect_d1(r);
+	q5_deliver_d1(r);
+}
