4da093fdef6aed081eee49dbf743430c266160c3
MattDiesel <M@ttDiesel.co.uk>
2015-03-11 10:50:15 +0000

    Implemented q4

--- routines/q4_collect_d1.cc
+++ routines/q4_collect_d1.cc
@@ -14,4 +14,16 @@ void q4_collect_d1(Robot& r) {
 	r.drive.stop();
 
 	conveyorCollect(r, shouldCollectAny);
+
+	auto line = r.ls.read();
+
+	if (line.state != LineSensors::Reading::JUNCTION) {
+		r.drive.straight(-0.1);
+
+		do {
+			line = r.ls.read();
+		} while (line.state != LineSensors::Reading::JUNCTION);
+
+		r.drive.stop();
+	}
 }
