de5799b34f02db1d6a17c6f8fcb848901581fc6d
MattDiesel <M@ttDiesel.co.uk>
2015-03-11 09:31:06 +0000

    Added Q4 routine

--- routines/q4_collect_d1.cc
+++ routines/q4_collect_d1.cc
@@ -2,5 +2,16 @@
 #include "util/tracker.h"
 #include "subroutines.h"
 
+
+bool shouldCollectAny(EggSensor::Reading& reading)
+{
+	return( reading.bestGuess != EGG_NONE );
+}
+
+
 void q4_collect_d1(Robot& r) {
+	goToConveyor(r, false);
+	r.drive.stop();
+
+	conveyorCollect(r, shouldCollectAny);
 }
