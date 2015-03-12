84d56572229c8bf1052d3a686f8db34ca92e2511
Eric Wieser <efw27@cam.ac.uk>
2015-03-12 12:16:19 +0000

    Catch exceptions to make destructors run

--- tests/t_all.cc
+++ tests/t_all.cc
@@ -8,6 +8,8 @@
 #include <string>
 #include <robot_delay.h>
 
+#include <typeinfo>
+
 
 
 int main() {
@@ -17,36 +19,42 @@ int main() {
 	std::cout << "initialised" << std::endl;
 
 	while(1) {
-		Robot r(link);
+		try {
+			Robot r(link);
 
-		std::cout
-			<< "Choose a start position:" << std::endl
-			<< " Q1 - collect D2 and D3     - start box, facing in" << std::endl
-			<< " Q2 - deliver D2 and D3     - last egg location" << std::endl
-			<< " Q3 - return from D2 and D3 - delivery crossroads, facing D3" << std::endl
-			<< " Q4 - collect remainder     - start box, facing out" << std::endl
-			<< " Q5 - deliver remainder     - last egg location" << std::endl
-			<< " Q3 - return from D1        - facing D1" << std::endl;
+			std::cout
+				<< "Choose a start position:" << std::endl
+				<< " Q1 - collect D2 and D3     - start box, facing in" << std::endl
+				<< " Q2 - deliver D2 and D3     - last egg location" << std::endl
+				<< " Q3 - return from D2 and D3 - delivery crossroads, facing D3" << std::endl
+				<< " Q4 - collect remainder     - start box, facing out" << std::endl
+				<< " Q5 - deliver remainder     - last egg location" << std::endl
+				<< " Q3 - return from D1        - facing D1" << std::endl;
 
-		std::cout << "Your choice: ";
-		int n = -1;
-		std::cin >> n;
+			std::cout << "Your choice: ";
+			int n = -1;
+			std::cin >> n;
 
-		if(n <= 1) q1_collect_d2d3(r);
-		if(n == 2) {
-			r.courier.recordEggAdded(EGG_BROWN);
-			r.courier.recordEggAdded(EGG_TASTY);
-			r.courier.recordEggAdded(EGG_BROWN);
+			if(n <= 1) q1_collect_d2d3(r);
+			if(n == 2) {
+				r.courier.recordEggAdded(EGG_BROWN);
+				r.courier.recordEggAdded(EGG_TASTY);
+				r.courier.recordEggAdded(EGG_BROWN);
+			}
+			if(n <= 2) q2_deliver_d2d3(r);
+			if(n <= 3) q3_return_from_d2d3(r);
+
+			if(n <= 4) q4_collect_d1(r);
+			if(n == 5) {
+				r.courier.recordEggAdded(EGG_WHITE);
+				r.courier.recordEggAdded(EGG_WHITE);
+			}
+			if(n <= 5) q5_deliver_d1(r);
+			if(n <= 6) q6_return_from_d1(r);
 		}
-		if(n <= 2) q2_deliver_d2d3(r);
-		if(n <= 3) q3_return_from_d2d3(r);
-
-		if(n <= 4) q4_collect_d1(r);
-		if(n == 5) {
-			r.courier.recordEggAdded(EGG_WHITE);
-			r.courier.recordEggAdded(EGG_WHITE);
+		catch(std::exception &e) {
+			std::cout << "Caught " << typeid(e).name() << ":" << e.what() << std::endl;
+			std::cout << "Restarting" << std::endl;
 		}
-		if(n <= 5) q5_deliver_d1(r);
-		if(n <= 6) q6_return_from_d1(r);
 	}
 }
