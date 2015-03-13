920a376e693b3b2cdc0e913363ae9bd2a022d07a
Eric Wieser <wieser.eric@gmail.com>
2015-03-13 14:30:50 +0000

    graphviz?

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.mathjax', 'breathe'
+	'sphinx.ext.mathjax', 'breathe', 'sphinx.ext.graphviz'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/routes.rst
+++ docs/routes.rst
@@ -1,4 +1,16 @@
 Navigation plan
 ===============
 
+.. graphviz::
+
+	digraph foo {
+		graph[rankdir=LR]
+
+		"Q1" -> "Q2" -> "Q3";
+		"Q3" -> "Q4";
+		"Q4" -> "Q5" -> "Q6";
+
+   		{ rank=same; Q1; Q4 }
+	}
+
 .. doxygenfile:: routines/subroutines.h
