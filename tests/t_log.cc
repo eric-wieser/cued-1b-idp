a2b3c2044c685cd049cd84eaf18d1cb614940158
Eric Wieser <wieser.eric@gmail.com>
2015-03-14 14:32:01 +0000

    Kill unused logging code

--- tests/t_log.cc
+++ tests/t_log.cc
@@ -5,12 +5,8 @@
 #include "util/logging.h"
 
 
-int main(int argc, char const *argv[])
-{
-	DEBUG(std::cout << "Debug" << std::endl);
-	INFO(std::cout << "Info" << std::endl);
-	WARN(std::cout << "Warn" << std::endl);
-	ERROR(std::cout << "Error" << std::endl);
+int main(int argc, char const *argv[]) {
+	std::cout << "Error" << std::endl);
 
 	Logger l("Section 1");
 	l << "Hi" << std::endl;
--- util/logging.h
+++ util/logging.h
@@ -1,43 +1,13 @@
 #pragma once
 
-#define LEVEL_DEBUG 1
-#define LEVEL_INFO 2
-#define LEVEL_WARN 3
-#define LEVEL_ERROR 4
-
-#ifndef LOGGING
-	#define LOGGING LEVEL_WARN
-#endif
-
-#if LOGGING <= LEVEL_DEBUG
-	#define DEBUG(x) (x)
-#else
-	#define DEBUG(x) 
-#endif
-
-#if LOGGING <= LEVEL_INFO
-	#define INFO(x) (x)
-#else
-	#define INFO(x) 
-#endif
-
-#if LOGGING <= LEVEL_WARN
-	#define WARN(x) (x)
-#else
-	#define WARN(x) 
-#endif
-
-#if LOGGING <= LEVEL_ERROR
-	#define ERROR(x) (x)
-#else
-	#define ERROR(x) 
-#endif
-
 #include <string>
 #include <iostream>
 
 class Robot;
 
+/**
+	Heirarchical logger, use to produce indented logs
+*/
 class Logger {
 public:
 	Logger* const parent;
@@ -47,9 +17,11 @@ private:
 public:
 	Logger(std::string name, Logger* _parent = NULL);
 
+	// move constructor only, to preserve `_active`
 	Logger(const Logger& other) = delete;
 	Logger(Logger&& other);
 
+	/// upon destruction, log either "[done]" or "[threw]"
 	~Logger() {
 		if(std::uncaught_exception())
 			*this << "[threw]" << std::endl;
@@ -58,20 +30,24 @@ public:
 		_active = this->parent;
 	}
 
+	/// create a sublogger of this logger
 	Logger child(std::string name) {
 		return Logger(name, this);
 	}
 
+	/// record a checkpoint. Scope for stopping the robot and waiting for user interaction
 	void checkpoint(Robot& r, std::string id) {
 		// r.drive.stop();
 		*this << "Checkpoint: " << id << std::endl;
 		// std::cin.get();
 	}
 
+	/// the depth of this logger - used for indentation
 	inline int depth() const {
 		return parent == NULL ? 1 : 1 + parent->depth();
 	}
 
+	/// get the current active logger
 	static Logger& active() {
 		if(_active == NULL) {
 			_active = new Logger("Unnamed logger");
@@ -83,7 +59,7 @@ public:
 	friend std::ostream& operator<< (Logger& logger, const T& t);
 };
 
-
+/// Output content to the logger, prefixed with appropriate indentation
 template<typename T>
 std::ostream& operator <<(Logger& logger, const T& t) {
 	return std::cout << logger._indent << t;
