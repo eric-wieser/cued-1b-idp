2a313ba638a7e9cf9bf378d715bddd70ca2400a1
Matt Diesel <M@ttDiesel.co.uk>
2015-03-16 14:07:47 +0000

    Trying @image instead of htmlonly

--- routines/subroutines.h
+++ routines/subroutines.h
@@ -8,11 +8,7 @@
 Start: Starting position
 End: Last position on conveyor
 
-\htmlonly
-<div align='center'>
-<embed src="http://1b-idp.readthedocs.org/en/latest/_static/q1_collect_d2d3.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" />
-</div>
-\endhtmlonly 
+@image html http://1b-idp.readthedocs.org/en/latest/_static/q1_collect_d2d3.svg
 */
 void q1_collect_d2d3(Robot& r);
 
