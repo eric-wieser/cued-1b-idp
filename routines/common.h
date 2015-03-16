5fd9560d222813c5fa493271cdbce371f8f8f683
Matt Diesel <M@ttDiesel.co.uk>
2015-03-16 13:49:59 +0000

    Testing adding an SVG diagram to doc headers

--- routines/common.h
+++ routines/common.h
@@ -3,12 +3,23 @@
 #include "dev/linesensors.h"
 #include "robot.h"
 
-typedef bool (*EGG_CALLBACK)(EggSensor::Reading& reading);
 
 struct negate { LineSensors::Reading::State s; };
 void waitForLine(Robot& r, LineSensors::Reading::State s);
 void waitForLine(Robot& r, negate n);
+
 void goToConveyor(Robot& r, bool east = true);
+
+typedef bool (*EGG_CALLBACK)(EggSensor::Reading& reading);
+
+/** Drives along the conveyor collecting eggs
+\htmlonly
+<div align='center'>
+<embed src="http://1b-idp.readthedocs.org/en/latest/_static/conveyorCollect.svg" type="image/svg+xml"     pluginspage="http://www.adobe.com/svg/viewer/install/" />
+</div>
+\endhtmlonly 
+*/
 void conveyorCollect(Robot& r, EGG_CALLBACK shouldCollect);
+
 void checkpoint(Robot& r, std::string id);
 void dropEggs(Robot& r, int n = 1);
